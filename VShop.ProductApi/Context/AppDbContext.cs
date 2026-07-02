02f989384e5c847517d329453c913056649ec978
BrenoMelo <brenoigor123@gmail.com>
2026-07-01 22:02:30 -0300

    First Commit, Aula Marcoratti

---
+++ VShop.ProductApi/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VShop.ProductApi.Models;
+
+namespace VShop.ProductApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        //Fluent API - Sobreescrita de convenções do EF Core
+    }
+}
